21ec7a0708070a7b41c0d35ed8d7608b11c86476
Pranav More <pranavmore2811@gmail.com>
2026-03-11 14:43:41 +0530

    fixed

--- src/components/CachedAPI.js
+++ src/components/CachedAPI.js
@@ -3,42 +3,30 @@ import React, { useState, useEffect, useMemo } from "react";
 function CachedAPI() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [limit, setLimit] = useState(5);
 
   useEffect(() => {
-    setLoading(true);
-
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
         setLoading(false);
       });
-  }, [limit]);
+  }, []);
 
   const cachedPosts = useMemo(() => {
-    return posts.slice(0, limit);
-  }, [posts, limit]);
+    return posts;
+  }, [posts]);
+
+  if (loading) return <p>Loading...</p>;
 
   return (
     <div>
-      <input
-        type="number"
-        value={limit}
-        onChange={(e) => setLimit(Number(e.target.value))}
-      />
-
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {cachedPosts.map((post) => (
-            <li key={post.id}>
-              {post.title}
-            </li>
-          ))}
-        </ul>
-      )}
+      {cachedPosts.map((post) => (
+        <div key={post.id}>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
